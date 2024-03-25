f5b004e96af4c1aa58167f3b5de2b05da2b47228
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2024-03-25 13:38:11 -0500

    Routes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+require('dotenv/config');
+
 const api = process.env.BASE_URL;
 const port = 5000;
 
 app.get("/", (req, res) => {
     res.send("hello world");
+});
+
+app.get(api + "/products", (req, res) => {
+    res.send("hello");
 });
 
 app.listen(port, () => {
