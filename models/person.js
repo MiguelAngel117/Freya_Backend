273a4a4e22dc520b7e023ba5a8e38830e0d947f6
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2024-03-29 13:01:16 -0500

    Entity Persons

---
+++ models/person.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const personSchema = mongoose.Schema({
+    name_user: String,
+    type_document: String,
+    number_document: String,
+    address: String,
+    city: String,
+    country: String,
+    department: String,
+    number_phone: String,
+    email: String
+})
+
+exports.Person = mongoose.model('persons', personSchema);
